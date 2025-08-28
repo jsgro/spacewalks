8ce4ccc5271f2b510ffe6ae5973191b138b47d8d
Jean-Yves Sgro <jsgro@wisc.edu>
2025-08-28 11:00:42 -0500

    Added new functionality to CSV output; using extra pytest features

--- tests/test_eva_data_analysis.py
+++ tests/test_eva_data_analysis.py
@@ -1,5 +1,9 @@
 import pytest
 from eva_data_analysis import text_to_duration
+from eva_data_analysis import calculate_crew_size
+
+def test_text_to_duration_integer():
+    assert text_to_duration("10:00") == 10
 
 def test_text_to_duration_irrational():
     assert text_to_duration("10:20") == pytest.approx(10.33333)
@@ -8,8 +12,20 @@ def test_text_to_duration_irrational():
 def test_text_to_duration_float():
     assert text_to_duration("10:15") == 10.25
 
-def test_text_to_duration_integer():
-    assert text_to_duration("10:00") == 10
+
 
 # test_text_to_duration_float()
 # test_text_to_duration_integer()
+# test_text_to_duration_irrational()
+
+@pytest.mark.parametrize("input_value, expected_result", [
+    ("Valentina Tereshkova;", 1),
+    ("Judith Resnik; Sally Ride;", 2),
+])
+def test_calculate_crew_size(input_value, expected_result):
+    """
+    Test that calculate_crew_size returns expected ground truth values
+    for typical crew values
+    """
+    actual_result = calculate_crew_size(input_value)
+    assert actual_result == expected_result
