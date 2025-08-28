0abee06f9d99ce29dd369eff45022e07b3497d77
Jean-Yves Sgro <jsgro@wisc.edu>
2025-08-28 10:27:23 -0500

    added all examples from class version

--- tests/test_eva_data_analysis.py
+++ tests/test_eva_data_analysis.py
@@ -1,7 +1,12 @@
+import pytest
 from eva_data_analysis import text_to_duration
 
+def test_text_to_duration_irrational():
+    assert text_to_duration("10:20") == pytest.approx(10.33333)
+   # assert abs(text_to_duration("10:20") - 10.33333333) < 1e-5
+
 def test_text_to_duration_float():
-    assert abs(text_to_duration("10:20") - 10.33333333) < 1e-5
+    assert text_to_duration("10:15") == 10.25
 
 def test_text_to_duration_integer():
     assert text_to_duration("10:00") == 10
